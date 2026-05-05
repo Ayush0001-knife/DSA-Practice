fc65a2d028109d14633c13913bac92be85acd7b2
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-05 10:00:00 +0530

    Array-Part-2

---
+++ Array-Part-2/Medium-31-Next-Permutation.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+// bool nextPermutation1(int nums[], int n)
+// {
+//       for (int i = n - 2; i >= 0; i--)
+//       {
+//             for (int j = i + 1; j < n; j++)
+//             {
+//                   if (nums[i] < nums[j])
+//                   {
+//                         swap(nums[i], nums[j]);
+//                         return true;
+//                   }
+//             }
+//       }
+//       return false;
+// }
+
+bool nextPermutation1(int nums[], int n)
+{
+      for (int j = n - 2; j >= 0; j--)
+      {
+            if (nums[j] < nums[j + 1])
+            {
+                  for (int i = n - 1; i > j; i--)
+                  {
+                        if (nums[i] > nums[j])
+                        {
+                              swap(nums[i], nums[j]);
+                              reverse(nums + j + 1, nums);
+                              return true;
+                        }
+                  }
+            }
+      }
+
+      return false;
+}
+
+int main()
+{
+
+      int nums[] = {1, 3, 2};
+      int n = sizeof(nums) / sizeof(nums[0]);
+
+      bool is_Largest = nextPermutation1(nums, n);
+
+      if (!is_Largest)
+      {
+            sort(nums, nums + n);
+      }
+
+      cout << "[ ";
+      for (int k = 0; k < n; k++)
+      {
+            cout << nums[k] << ",";
+      }
+      cout << " ]" << endl;
+
+      return 0;
+}
