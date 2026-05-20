6d556404dba6ca6310d299026c057365ded27707
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-20 10:00:00 +0530

    Array-Part-2

--- Array-Part-2/Medium-11-Container-With-Most-Water.cpp
+++ Array-Part-2/Medium-11-Container-With-Most-Water.cpp
@@ -5,6 +5,19 @@ int main()
 {
       int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
       int n = sizeof(height) / sizeof(height[0]);
+      int maxVolume = 0;
+
+      for (int i = 0; i < n; i++)
+      {
+            int currVol = 0;
+            for (int j = i + 1; j < n; j++)
+            {
+                  currVol = max((min(height[i], height[j]) * (j - i)), currVol);
+            }
+            maxVolume = max(maxVolume, currVol);
+      }
+
+      cout << "Total water Trapped : " << maxVolume << endl;
 
       return 0;
 }
