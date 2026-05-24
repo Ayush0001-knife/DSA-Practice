f5eda92a96c9d115aadcdfd00d3199caa462ac38
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-24 20:00:00 +0530

    Array-Part-1

--- Array-Part-1/Easy-121-Best-Time-To-Buy-And-Sell-Stock.cpp
+++ Array-Part-1/Easy-121-Best-Time-To-Buy-And-Sell-Stock.cpp
@@ -36,27 +36,28 @@ int optimizedBruteForceApproach(int prices[], int n)
       return max_profit;
 }
 
+int greedyApproach(int prices[], int n)
+{
+      int min_price = prices[0];
+      int max_profit = 0;
+
+      for (int i = 1; i < n; i++)
+      {
+            min_price = min(min_price, prices[i]);
+            int curr_profit = prices[i] - min_price;
+            max_profit = max(max_profit, curr_profit);
+      }
+      return max_profit;
+}
+
 int main()
 {
       int prices[] = {7, 1, 5, 3, 6, 4};
       int n = sizeof(prices) / sizeof(prices[0]);
 
-      int minPrice = prices[0];
-      int maxProfit = 0;
+      int max_profit = greedyApproach(prices, n);
 
-      for (int i = 1; i < n; i++)
-      {
-            if (minPrice > prices[i])
-            {
-                  minPrice = prices[i];
-            }
-            else
-            {
-                  maxProfit = max(maxProfit, prices[i] - minPrice);
-            }
-      }
-
-      cout << maxProfit << endl;
+      cout << "Max Profit : " << max_profit;
 
       return 0;
 }
