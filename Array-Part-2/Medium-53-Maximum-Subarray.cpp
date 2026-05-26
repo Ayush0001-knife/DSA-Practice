22a3a78d0c68762adebd55f3e266ff2ec2435c65
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-26 20:00:00 +0530

    Array-Part-2

--- Array-Part-2/Medium-53-Maximum-Subarray.cpp
+++ Array-Part-2/Medium-53-Maximum-Subarray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // This is The Brute Force Approach with time complexity O(n^3)
-int bruteForceApproach(int arr[], int n)
+int bruteForceApproach(int nums[], int n)
 {
       int max_sum = INT16_MIN;
 
@@ -13,7 +13,7 @@ int bruteForceApproach(int arr[], int n)
                   int curr_sum = 0;
                   for (int k = i; k <= j; k++)
                   {
-                        curr_sum += arr[k];
+                        curr_sum += nums[k];
                   }
                   if (curr_sum > max_sum)
                   {
@@ -25,7 +25,7 @@ int bruteForceApproach(int arr[], int n)
 }
 
 // This is the Optimized Brute Force Approach with time complexity O(n^2)
-int optimizedBruteForceApproach(int arr[], int n)
+int optimizedBruteForceApproach(int nums[], int n)
 {
       int max_sum = INT16_MIN;
 
@@ -34,7 +34,7 @@ int optimizedBruteForceApproach(int arr[], int n)
             int curr_sum = 0;
             for (int j = i; j < n; j++)
             {
-                  curr_sum += arr[j];
+                  curr_sum += nums[j];
                   max_sum = max(max_sum, curr_sum);
             }
       }
@@ -42,13 +42,13 @@ int optimizedBruteForceApproach(int arr[], int n)
 }
 
 // This is the Kadane's Algorithm based Approach with time complexity O(n)
-int kadanesApproach(int arr[], int n)
+int kadanesApproach(int nums[], int n)
 {
       int curr_sum = 0, max_sum = INT16_MIN;
 
       for (int i = 0; i < n; i++)
       {
-            curr_sum += arr[i];
+            curr_sum += nums[i];
             max_sum = max(curr_sum, max_sum);
             if (curr_sum < 0)
             {
@@ -60,13 +60,13 @@ int kadanesApproach(int arr[], int n)
 
 int main()
 {
-      int arr[] = {2, -3, 6, -5, 4, 2};
-      // int arr[] = {1, 2, 3, 4, 5};
-      int n = sizeof(arr) / sizeof(arr[0]);
+      int nums[] = {2, -3, 6, -5, 4, 2};
+      // int nums[] = {1, 2, 3, 4, 5};
+      int n = sizeof(nums) / sizeof(nums[0]);
 
-      // int max_sum = bruteForceApproach(arr, n);
-      // int max_sum = optimizedBruteForceApproach(arr, n);
-      int max_sum = kadanesApproach(arr, n);
+      // int max_sum = bruteForceApproach(nums, n);
+      // int max_sum = optimizedBruteForceApproach(nums, n);
+      int max_sum = kadanesApproach(nums, n);
 
       cout << max_sum << endl;
       return 0;
