6945bddf0442e98320dab67ab8af7f5d2f18bc66
Ayush Gautam <ayushstudy647@gmail.com>
2026-06-07 10:00:00 +0530

    Array-Part-2

---
+++ Array-Part-2/Medium-15-3-Sum.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      int nums[] = {-1, 0, 1, 2, -1, -4};
+      int n = sizeof(nums) / sizeof(nums[0]);
+      sort(nums, nums + n);
+      vector<vector<int>> ans;
+
+      for (int i = 0; i < n; i++)
+      {
+
+            // Skip duplicate first element
+            if (i > 0 && nums[i] == nums[i - 1])
+                  continue;
+
+            int st = i + 1;
+            int end = n - 1;
+
+            while (st < end)
+            {
+
+                  int sum = nums[i] + nums[st] + nums[end];
+
+                  if (sum == 0)
+                  {
+
+                        ans.push_back({nums[i], nums[st], nums[end]});
+
+                        st++;
+                        end--;
+
+                        // Skip duplicate left values
+                        while (st < end && nums[st] == nums[st - 1])
+                              st++;
+
+                        // Skip duplicate right values
+                        while (st < end && nums[end] == nums[end + 1])
+                              end--;
+                  }
+
+                  else if (sum < 0)
+                  {
+                        st++;
+                  }
+
+                  else
+                  {
+                        end--;
+                  }
+            }
+      }
+
+      return 0;
+}
