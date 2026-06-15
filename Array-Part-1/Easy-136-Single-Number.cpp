037900e9e6a786f0beaee853f0665c80d51d1933
Ayush Gautam <ayushstudy647@gmail.com>
2026-06-15 21:44:18 +0530

    Array-Part-1

--- Array-Part-1/Easy-136-Single-Number.cpp
+++ Array-Part-1/Easy-136-Single-Number.cpp
@@ -7,35 +7,14 @@ int main()
       int nums[] = {1};
       int n = sizeof(nums) / sizeof(nums[0]);
 
-      int max_element = INT16_MIN;
+      int ans = 0;
 
       for (int i = 0; i < n; i++)
       {
-            max_element = max(max_element, nums[i]);
+            ans ^= nums[i];
       }
 
-      vector<int> freqArr(max_element + 1, 0);
-
-      for (int i = 0; i < n; i++)
-      {
-            freqArr[nums[i]]++;
-      }
-
-      for (int i = 0; i < max_element + 1; i++)
-      {
-            cout << freqArr[i] << " ";
-      }
-
-      cout << endl;
-
-      for (int i = 0; i < max_element + 1; i++)
-      {
-            if (freqArr[i] == 1)
-            {
-                  cout << i << endl;
-                  break;
-            }
-      }
+      cout << ans << endl;
 
       return 0;
 }
