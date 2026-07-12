30abbab3dce5cc1fd16134838039aea36cc628e8
Ayush Gautam <ayushstudy647@gmail.com>
2026-07-12 10:00:00 +0530

    Array-Part-2

--- Array-Part-2/Medium-79-Word-Search.cpp
+++ Array-Part-2/Medium-79-Word-Search.cpp
@@ -12,73 +12,15 @@ int main()
       int cols = board[0].size();
 
       string word = "ABCCED";
-      int count = 0;
       string newWord = "";
+      int count = 0;
 
       for (int i = 0; i < rows; i++)
       {
             for (int j = 0; j < cols; j++)
             {
-                  if (board[i][j] == word[count])
-                  {
-                        newWord = newWord + board[i][j];
-                        int r = i,
-                            c = j;
-                        count += 1;
-                        bool found = false;
-                        // vertical down
-                        if (found != true)
-                        {
-                              for (int k = r + 1; k < rows; k++)
-                              {
-                                    if (board[k][c] == word[count])
-                                    {
-                                          newWord = newWord + board[k][c];
-                                          count += 1;
-                                          found = true;
-                                          r = k;
-                                          c = j;
-                                    }
-                              }
-                        }
-                        // horizontal left
-                        if (found != true)
-                        {
-                              for (int k = c - 1; k >= 0; k--)
-                              {
-                                    if (board[r][k] == word[count])
-                                    {
-                                          newWord = newWord + board[r][k];
-                                          count += 1;
-                                          found = true;
-                                          j = k;
-                                    }
-                              }
-                        }
-                        // horizontal right
-                        if (found != true)
-                        {
-                              for (int k = c + 1; k < cols; k++)
-                              {
-                                    if (board[r][k] == word[count])
-                                    {
-                                          newWord = newWord + board[r][k];
-                                          count += 1;
-                                          found = true;
-                                          j = k;
-                                    }
-                              }
-                        }
-                        if (found == false)
-                        {
-                              newWord.pop_back();
-                              count -= 1;
-                        }
-                  }
             }
       }
-
-      cout << newWord;
 
       return 0;
 }
