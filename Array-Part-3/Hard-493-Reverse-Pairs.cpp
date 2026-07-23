dca9843b273b1c748e17d8fac574e4621bc189b7
Ayush Gautam <ayushstudy647@gmail.com>
2026-07-23 10:00:00 +0530

    Array-Part-3

--- Array-Part-3/Hard-493-Reverse-Pairs.cpp
+++ Array-Part-3/Hard-493-Reverse-Pairs.cpp
@@ -3,9 +3,30 @@ using namespace std;
 
 int main()
 {
-      int nums[] = {2, 4, 3, 5, 1};
-      int n = sizeof(nums) / sizeof(nums[0]);
+      vector<int> nums = {1, 3, 2, 3, 1};
+      int len = nums.size();
       int count = 0;
+
+      std::sort(nums.begin(), nums.end());
+
+      for (int i = len - 1; i >= 1; i--)
+      {
+            for (int j = 0; j < len; j++)
+            {
+                  if (nums[i] > (2 * nums[j]))
+                  {
+                        count += 1;
+                  }
+                  else
+                  {
+                        if (i == len - 1 && j == 0)
+                        {
+                              return count;
+                        }
+                        break;
+                  }
+            }
+      }
 
       // Brute Force
       // for (int i = 0; i < n - 1; i++)
